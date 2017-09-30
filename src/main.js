5d460b9337f9f1b6810f05d46ecf98934f6a8a52
Lena Kociemba <Lena@Zurs-MacBook-Pro.local>
2017-09-30 22:30:18 +0200

    Fix database and undefined error

--- src/main.js
+++ src/main.js
@@ -10,22 +10,24 @@ async function main() {
   await config_handler.loadConfig('config.json');
   await bot.connect("ai-issuebot");
   bot.on('receive', async(data) => {
-     // Weiterverarbeitung
-     var reactiontype = texterkennung.reaction(data);
-     var userid = data.user;
-     if(reactiontype === 1){
-        var issues = await database.issue.listBySlackUserID(userid);
-        if(issues.length > 0){
-           bot.send("Issues: " + issues[0].url);
+      // Weiterverarbeitung
+      if(data.text){
+        var reactiontype = texterkennung.reaction(data);
+        var userid = data.user;
+        if(reactiontype === 1){
+           var issues = await database.issue.listBySlackUserID(userid);
+           if(issues.length > 0){
+              bot.send("Issues: " + issues[0].url);
+           }
+           bot.send("Langeweile: Anzahl der offenen Issues " + issues.length);
+        } else if (reactiontype === 2) {
+           bot.send("<@here>; Ich bin noch da.");
+        } else if (reactiontype === 3) {
+           var issues = await database.issue.listBySlackUserID(userid);
+           bot.send("Vorschlag: Anzahl der offenen Issues " + issues.length);
         }
-        bot.send("Langeweile: Anzahl der offenen Issues " + issues.length);
-     } else if (reactiontype === 2) {
-        bot.send("<@here>; Ich bin noch da.");
-     } else if (reactiontype === 3) {
-        var issues = await database.issue.listBySlackUserID(userid);
-        bot.send("Vorschlag: Anzahl der offenen Issues " + issues.length);
      }
-  });
+   });
 }
 
 
