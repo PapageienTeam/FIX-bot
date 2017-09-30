74b26f2c5dc734f5d90ea0559427721537b90bfc
Lena Kociemba <Lena@Zurs-MacBook-Pro.local>
2017-09-30 22:39:21 +0200

    Fix

--- src/main.js
+++ src/main.js
@@ -6,27 +6,31 @@ const texterkennung = require('./textanalysis');
 const database = require('./db');
 
 async function main() {
-  console.log("Start");
-  await config_handler.loadConfig('config.json');
-  await bot.connect("ai-issuebot");
-  bot.on('receive', async(data) => {
+   console.log("Start");
+   await config_handler.loadConfig('config.json');
+   await bot.connect("ai-issuebot");
+   bot.on('receive', async(data) => {
       // Weiterverarbeitung
       if(data.text){
-        var reactiontype = texterkennung.reaction(data);
-        var userid = data.user;
-        if(reactiontype === 1){
-           var issues = await database.issue.listBySlackUserID(userid);
-           if(issues.length > 0){
-              bot.send("Issues: " + issues[0].url);
-           }
-           bot.send("Langeweile: Anzahl der offenen Issues " + issues.length);
-        } else if (reactiontype === 2) {
-           bot.send("<@here>; Ich bin noch da.");
-        } else if (reactiontype === 3) {
-           var issues = await database.issue.listBySlackUserID(userid);
-           bot.send("Vorschlag: Anzahl der offenen Issues " + issues.length);
-        }
-     }
+         var reactiontype = texterkennung.reaction(data);
+         var userid = data.user;
+         if(reactiontype === 1){
+            var issues = await database.issue.listBySlackUserID(userid);
+            if(issues.length > 0){
+               bot.send("Issues: " + issues[0].url);
+            }
+            bot.send("Langeweile: Anzahl der offenen Issues " + issues.length);
+         } else if (reactiontype === 2) {
+            bot.send("<@here>; Ich bin noch da.");
+         } else if (reactiontype === 3) {
+            var issues = await database.issue.listBySlackUserID(userid);
+            if(issues.length > 0){
+               bot.send("Issues: " + issues[0].url);
+            } else {
+               bot.send("Keine offenen Issues");
+            }
+         }
+      }
    });
 }
 
@@ -34,14 +38,14 @@ async function main() {
 
 
 if (require.main===module){
-  process.on('unhandledRejection', err => {
-    console.log(err);
-    process.exit(1);
-  });
-  main();
+   process.on('unhandledRejection', err => {
+      console.log(err);
+      process.exit(1);
+   });
+   main();
 } else {
-  module.exports ={
-    db: require('./db'),
-    config_handler: require('./config_handler')
-  };
+   module.exports ={
+      db: require('./db'),
+      config_handler: require('./config_handler')
+   };
 }
