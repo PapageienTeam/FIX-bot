5f550a063242634aaf3c0e7b0025da23773033f9
Alexander Hoischen <alexander.hoischen@gmx.net>
2017-10-02 08:04:34 +0200

    Remove filesystem dependency in config tests
    Depending on the filesystem during unit tests is not too bad, but I'd
    still consider it an integration test. It also slows down testing by a
    random amount of time. I refactored config_handler to perform its logic
    in a separately testable function and skipped the 'integration' test by
    default.
    
    I also rewrote the assertions in the original test to use jest's expect
    instead of throwing errors. This is both more compact and provides
    better output on failures.

--- test/config_handler_test.js
+++ test/config_handler_test.js
@@ -1,7 +1,8 @@
 const config_handler = require("../src/config_handler")
 const fs = require('fs-extra');
 
-test("loadConfig", async () => {
+// This test depends on the filesystem, so it is skipped by default.
+test.skip("loadConfig", async () => {
   let TestConfig = JSON.stringify({
     PGUSER: 'postgres',
     PGHOST: 'localhost',
@@ -13,26 +14,25 @@ test("loadConfig", async () => {
   });
   await fs.writeFile('config_test.json', TestConfig);
   let _ = await config_handler.loadConfig('config_test.json');
-  if(process.env['PGUSER'] !== 'postgres'){
+  expect(process.env['PGUSER']).toBe('postgres');
+  expect(process.env['PGHOST']).toBe('localhost');
+  expect(process.env['PGPORT']).toBe('5050');
+  expect(process.env['PGPASSWORD']).toBe('test123');
+  expect(process.env['PGDATABASE']).toBe('gitbot');
+  expect(process.env['GIT_ORGANISATION']).toBe('Team');
+  expect(process.env['SLACK_BOT_TOKEN']).toBe('token');
+})
 
-    throw new Error('Flamingo');
-  }
-  if(process.env['PGHOST'] !== 'localhost'){
-    throw new Error('Flamingo');
-  }
-  if(process.env['PGPORT'] !== '5050') {
-    throw new Error('Flamingo');
-  }
-  if(process.env['PGPASSWORD'] !== 'test123') {
-    throw new Error('Flamingo');
-  }
-  if(process.env['PGDATABASE'] !== 'gitbot') {
-    throw new Error('Flamingo');
-  }
-  if(process.env['GIT_ORGANISATION'] !== 'Team') {
-    throw new Error('Flamingo');
-  }
-  if(process.env['SLACK_BOT_TOKEN'] !== 'token') {
-    throw new Error('Flamingo');
-  }
+test("Loads configuration into environment", () => {
+  const testConfig = {
+    'User': 'User1',
+    'Password': '123456',
+    'OtherData': '',
+  };
+
+  config_handler.loadConfigIntoEnvironment(testConfig);
+
+  expect(process.env['User']).toBe('User1');
+  expect(process.env['Password']).toBe('123456');
+  expect(process.env['OtherData']).toBe('');
 })
